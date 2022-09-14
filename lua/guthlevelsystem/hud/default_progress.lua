e1e075ebf167e48c720c272169e80c7bb6d226a3
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-14 17:49:22 +0200

    [refactor] huds config naming convention

--- lua/guthlevelsystem/hud/default_progress.lua
+++ lua/guthlevelsystem/hud/default_progress.lua
@@ -1,70 +1,75 @@
-local config = {
-    SmoothSpeed = 5, --  animation speed for progress box & color changes
+local config = {}
+config.smooth_speed = 5 --  animation speed for progress box & color changes
 
-    --  box
-    BoxY = ScrH() * .025, --  box's Y coordinate; by default set to 2.5% of the screen height  
-    BoxW = ScrW() * .2, --  box's width; by default set to 20% of the screen width
-    BoxH = ScrH() * .025, --  box's height; by default set to 2.5% of the screen height
+--  box
+config.box_w = ScrW() * .2 --  box's width; by default set to 20% of the screen width
+config.box_h = ScrH() * .025 --  box's height; by default set to 2.5% of the screen height
+config.box_x = ScrW() / 2 - config.box_w / 2  --  box's X coordinate; by default it's centered
+config.box_y = ScrH() * .025 --  box's Y coordinate; by default set to 2.5% of the screen height  
 
-    --  progress box
-    BoxProgressPadding = 3, --  gap at top, bottom, left & right of progress box
-    BoxProgressCornerRadius = 6, --  radius of box's corners; 0: no corner
-    BoxProgressColor = Color( 12, 234, 75, 150 ), --  color used in the progression box
-    IsBoxProgressColorByTeam = false, --  do we set the progress color to the current player's team color?
-                                      --  please note that setting this to true will not take in account BoxProgressColor value 
+--  progress box
+config.box_progress_padding = 3 --  gap at top, bottom, left & right of progress box
+config.box_progress_corner_radius = 6 --  radius of box's corners; 0: no corner
+config.box_progress_color = Color( 12, 234, 75, 150 ) --  color used in the progression box
+config.is_box_progress_color_by_team = false --  do we set the progress color to the current player's team color?
+                                      --  please note that setting this to true will not take in account box_progress_color value 
 
-    --  background box
-    BoxBackgroundCornerRadius = 6, --  radius of box's corners; 0: no corner
-    BoxBackgroundColor = Color( 130, 130, 130, 150 ), -- box background color
+--  background box
+config.box_background_corner_radius = 6 --  radius of box's corners; 0: no corner
+config.box_background_color = Color( 130, 130, 130, 150 ) -- box background color
 
-    --  texts
-    TextPadding = 8, --  gap at left and right of both texts; default: 8px
-    TextPrestige = "Prestige %d", --  prestige text format, if `guthlevelsystem.settings.prestige.enabled` is `true`; "%d" will be replaced with the prestige
-    TextLevel = "Level %d", --  level text format; "%d" will be replaced with the level
-    TextProgress = "%d%%", --  progress text format; "%d" will be replaced with the xp percent & "%%" will become automatically a "%"
-    TextIsProgress = false, 
-    TextFont = "Trebuchet22", --  font of both texts, see https://wiki.facepunch.com/gmod/Default_Fonts for default fonts
-    TextColor = Color( 255, 255, 255, 150 ), --  color of both texts
-}
+--  texts
+config.text_padding = 8 --  gap at left and right of both texts; default: 8px
+config.text_prestige = "Prestige %d" --  prestige text format, if `guthlevelsystem.settings.prestige.enabled` is `true`; "%d" will be replaced with the prestige
+config.text_level = "Level %d" --  level text format; "%d" will be replaced with the level
+config.text_progress = "%d%%" --  progress text format; "%d" will be replaced with the xp percent & "%%" will become automatically a "%"
+config.text_is_progress = false 
+config.text_font = "Trebuchet22" --  font of both texts, see https://wiki.facepunch.com/gmod/Default_Fonts for default fonts
+config.text_color = Color( 255, 255, 255, 150 ) --  color of both texts
 
-if config.TextFont == "Trebuchet22" then
+--
+--  end of config
+--
+
+--  create the default font
+if config.text_font == "Trebuchet22" then
     surface.CreateFont( "Trebuchet22", {
         font = "Trebuchet MS",
         size = 22,
     } )
 end
 
+--  HUD code
 local function lerp_color( t, a, b )
     return Color( Lerp( t, a.r, b.r ), Lerp( t, a.g, b.g ), Lerp( t, a.b, b.b ), Lerp( t, a.a, b.a ) )
 end
 
-local progress_color = config.BoxProgressColor
+local progress_color = config.box_progress_color
 local xp = 0
 guthlevelsystem.add_hud( "default_progress", function( ply )
     --  variables
-    local x = ScrW() / 2 - config.BoxW / 2
-    xp = Lerp( FrameTime() * config.SmoothSpeed, xp, ply:gls_get_xp() )
+    xp = Lerp( FrameTime() * config.smooth_speed, xp, ply:gls_get_xp() )
 
     local nxp = ply:gls_get_nxp()
     local ratio = math.Clamp( xp / nxp, 0, 1 )
     
-    if config.IsBoxProgressColorByTeam then
-        progress_color = lerp_color( FrameTime() * config.SmoothSpeed, progress_color, ColorAlpha( team.GetColor( ply:Team() ), progress_color.a ) )
+    if config.is_box_progress_color_by_team then
+        progress_color = lerp_color( FrameTime() * config.smooth_speed, progress_color, ColorAlpha( team.GetColor( ply:Team() ), progress_color.a ) )
     end
 
     --  background
-    draw.RoundedBox( config.BoxBackgroundCornerRadius, x, config.BoxY, config.BoxW, config.BoxH, config.BoxBackgroundColor )
+    draw.RoundedBox( config.box_background_corner_radius, config.box_x, config.box_y, config.box_w, config.box_h, config.box_background_color )
 
     --  progress
-    draw.RoundedBox( config.BoxProgressCornerRadius, x + config.BoxProgressPadding, config.BoxY + config.BoxProgressPadding, math.max( 0, config.BoxW * ratio - config.BoxProgressPadding * 2 ), config.BoxH - config.BoxProgressPadding * 2, progress_color )
+    draw.RoundedBox( config.box_progress_corner_radius, config.box_x + config.box_progress_padding, config.box_y + config.box_progress_padding, math.max( 0, config.box_w * ratio - config.box_progress_padding * 2 ), config.box_h - config.box_progress_padding * 2, progress_color )
 
     --  text
-    local right_text = config.TextIsProgress and config.TextProgress:format( ratio * 100 ) or ( "%d/%d XP" ):format( xp, nxp )
-    draw.SimpleText( config.TextLevel:format( ply:gls_get_level() ), config.TextFont, x + config.TextPadding, config.BoxY + config.BoxH / 2, config.TextColor, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
-    draw.SimpleText( right_text, config.TextFont, x + config.BoxW - config.TextPadding, config.BoxY + config.BoxH / 2, config.TextColor, TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER )
+    local right_text = config.text_is_progress and config.text_progress:format( ratio * 100 ) or ( "%d/%d XP" ):format( xp, nxp )
+    draw.SimpleText( config.text_level:format( ply:gls_get_level() ), config.text_font, config.box_x + config.text_padding, config.box_y + config.box_h / 2, config.text_color, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+    draw.SimpleText( right_text, config.text_font, config.box_x + config.box_w - config.text_padding, config.box_y + config.box_h / 2, config.text_color, TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER )
 
     --  prestige
     if guthlevelsystem.settings.prestige.enabled then
-        draw.SimpleText( config.TextPrestige:format( ply:gls_get_prestige() ), config.TextFont, x + config.BoxW / 2, config.BoxY + config.BoxH / 2, config.TextColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+        draw.SimpleText( config.text_prestige:format( ply:gls_get_prestige() ), config.text_font, config.box_x + config.box_w / 2, config.box_y + config.box_h / 2, config.text_color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
     end
 end )
--- lua/guthlevelsystem/hud/default_text.lua
+++ lua/guthlevelsystem/hud/default_text.lua
@@ -1,36 +1,39 @@
-local config = {
-	--  position
-	PrestigeX = 30, --  X coordinate
-	PrestigeY = ScrH() * .71, --  Y coordinate
-	LevelX = 30, --  X coordinate
-	LevelY = ScrH() * .75, -- Y coordinate
-	XPX = 30, --  X coordinate
-	XPY = ScrH() * .79, --  Y coordinate
+local config = {}
 
-	--  texts
-	TextPrestige = "Prestige: %d", --  prestige text format, if `guthlevelsystem.settings.prestige.enabled` is `true`; "%d" will be replaced with the prestige
-    TextLevel = "Level: %d", --  level text format; "%d" will be replaced with the level
-	TextXP = "XP: %s", --  text format
-	XPIsPercent = false,--  do we show a percent or a text on the format of "XP/NXP"
-						--  e.g.: XPIsPercent = true => "XP : 50%"
-						--        XPIsPercent = false => "XP : 100/200"
-	TextFont =   "DermaLarge", --  font of both texts, see https://wiki.facepunch.com/gmod/Default_Fonts for default fonts
-	TextColor = Color( 255, 255, 255, 150 ), --  color of both texts
-}
+--  position
+config.prestige_x = 30 --  X coordinate
+config.prestige_y = ScrH() * .71 --  Y coordinate
+config.level_x = 30 --  X coordinate
+config.level_y = ScrH() * .75 -- Y coordinate
+config.xp_x = 30 --  X coordinate
+config.xp_y = ScrH() * .79 --  Y coordinate
 
+--  texts
+config.text_prestige = "Prestige: %d" --  prestige text format, if `guthlevelsystem.settings.prestige.enabled` is `true`; "%d" will be replaced with the prestige
+config.text_level = "Level: %d" --  level text format; "%d" will be replaced with the level
+config.text_xp = "XP: %s" --  text format
+config.xp_is_percent = false  --  do we show a percent or a text on the format of "XP/NXP"
+					   --  e.g.: xp_is_percent = true => "XP : 50%"
+					   --        xp_is_percent = false => "XP : 100/200"
+config.text_font = "DermaLarge" --  font of both texts, see https://wiki.facepunch.com/gmod/Default_Fonts for default fonts
+config.text_color = Color( 255, 255, 255, 150 ) --  color of both texts
+
+--
+--  end of config
+--
 
 local _xp = 0
 guthlevelsystem.add_hud( "default_text", function( ply )
 	_xp = Lerp( FrameTime() * 5, _xp or 0, ply:gls_get_xp() )
 
 	local level, nxp = ply:gls_get_level(), ply:gls_get_nxp()
-	local xp_text = config.XPIsPercent and config.TextXP:format( math.Round( _xp / nxp * 100 ) .. "%" ) or config.TextXP:format( math.Round( _xp ) .. "/" .. nxp )
+	local xp_text = config.xp_is_percent and config.text_xp:format( math.Round( _xp / nxp * 100 ) .. "%" ) or config.text_xp:format( math.Round( _xp ) .. "/" .. nxp )
 
 	--  text
-	draw.SimpleText( config.TextLevel:format( level ), config.TextFont, config.LevelX, config.LevelY, config.TextColor )
-	draw.SimpleText( xp_text, config.TextFont, config.XPX, config.XPY, config.TextColor )
+	draw.SimpleText( config.text_level:format( level ), config.text_font, config.level_x, config.level_y, config.text_color )
+	draw.SimpleText( xp_text, config.text_font, config.xp_x, config.xp_y, config.text_color )
 	
 	if guthlevelsystem.settings.prestige.enabled then
-		draw.SimpleText( config.TextPrestige:format( ply:gls_get_prestige() ), config.TextFont, config.PrestigeX, config.PrestigeY, config.TextColor )
+		draw.SimpleText( config.text_prestige:format( ply:gls_get_prestige() ), config.text_font, config.prestige_x, config.prestige_y, config.text_color )
 	end
 end )
